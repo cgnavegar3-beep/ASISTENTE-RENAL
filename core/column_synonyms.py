b0ec0053f422b867bfb51a8d7505635dff43348a
cgnavegar3-beep <cgnavegar3@gmail.com>
2026-04-03 11:53:30 +0200

    Create column_synonyms.py

---
+++ core/column_synonyms.py
@@ -0,0 +1,166 @@
+COLUMN_SYNONYMS = {
+    # -------------------------
+    # PACIENTE / IDENTIFICACIÓN
+    # -------------------------
+    "ID_REGISTRO": [
+        "paciente",
+        "id paciente",
+        "registro",
+        "id registro"
+    ],
+
+    # -------------------------
+    # MEDICACIÓN
+    # -------------------------
+    "Nº_TOTAL_MEDS_PAC": [
+        "numero total medicamentos",
+        "total medicamentos",
+        "numero de medicamentos",
+        "medicacion total",
+        "validaciones totales"
+    ],
+
+    # -------------------------
+    # FILTRADO GLOMERULAR CG
+    # -------------------------
+    "FG_CG": [
+        "fg cg",
+        "cockcroft gault",
+        "filtrado glomerular cg",
+        "fg cockcroft gault"
+    ],
+
+    "Nº_TOT_AFEC_CG": [
+        "medicamentos afectados cg",
+        "afectados cg",
+        "total afectados cg"
+    ],
+
+    "Nº_PRECAU_CG": [
+        "precaucion cg",
+        "medicamentos precaucion cg"
+    ],
+
+    "Nº_AJUSTE_DOS_CG": [
+        "ajuste dosis cg",
+        "ajuste cg"
+    ],
+
+    "Nº_TOXICID_CG": [
+        "toxicidad cg",
+        "medicamentos toxicidad cg"
+    ],
+
+    "Nº_CONTRAIND_CG": [
+        "contraindicados cg",
+        "contraindicado cg"
+    ],
+
+    "CAT_RIESGO_CG": [
+        "categoria riesgo cg",
+        "categoria cg"
+    ],
+
+    "RIESGO_CG": [
+        "riesgo cg",
+        "riesgo cockcroft gault"
+    ],
+
+    "NIVEL_ADE_CG": [
+        "nivel riesgo cg",
+        "nivel ade cg",
+        "grado riesgo cg"
+    ],
+
+    # -------------------------
+    # MDRD
+    # -------------------------
+    "FG_MDRD": [
+        "fg mdrd",
+        "filtrado mdrd"
+    ],
+
+    "Nº_TOT_AFEC_MDRD": [
+        "afectados mdrd",
+        "medicamentos afectados mdrd"
+    ],
+
+    "Nº_PRECAU_MDRD": [
+        "precaucion mdrd"
+    ],
+
+    "Nº_AJUSTE_DOS_MDRD": [
+        "ajuste dosis mdrd"
+    ],
+
+    "Nº_TOXICID_MDRD": [
+        "toxicidad mdrd"
+    ],
+
+    "Nº_CONTRAIND_MDRD": [
+        "contraindicados mdrd"
+    ],
+
+    "CAT_RIESGO_MDRD": [
+        "categoria riesgo mdrd"
+    ],
+
+    "RIESGO_MDRD": [
+        "riesgo mdrd"
+    ],
+
+    "NIVEL_ADE_MDRD": [
+        "nivel riesgo mdrd",
+        "nivel ade mdrd"
+    ],
+
+    # -------------------------
+    # CKD
+    # -------------------------
+    "FG_CKD": [
+        "fg ckd",
+        "filtrado ckd"
+    ],
+
+    "Nº_TOT_AFEC_CKD": [
+        "afectados ckd"
+    ],
+
+    "Nº_PRECAU_CKD": [
+        "precaucion ckd"
+    ],
+
+    "Nº_AJUSTE_DOS_CKD": [
+        "ajuste dosis ckd"
+    ],
+
+    "Nº_TOXICID_CKD": [
+        "toxicidad ckd"
+    ],
+
+    "Nº_CONTRAIND_CKD": [
+        "contraindicados ckd"
+    ],
+
+    "CAT_RIESGO_CKD": [
+        "categoria riesgo ckd"
+    ],
+
+    "RIESGO_CKD": [
+        "riesgo ckd"
+    ],
+
+    "NIVEL_ADE_CKD": [
+        "nivel riesgo ckd",
+        "nivel ade ckd"
+    ],
+
+    # -------------------------
+    # DECISIÓN CLÍNICA
+    # -------------------------
+    "ACEPTACION_MEDICO": [
+        "aceptacion medico",
+        "aceptacion map",
+        "propuestas aceptadas"
+    ]
+}
