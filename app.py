4c3966409ee50a52096c0441e1b99e3560097bd4
cgnavegar3-beep <cgnavegar3@gmail.com>
2026-04-13 18:05:28 +0200

    13 ABR 1805

--- app.py
+++ app.py
@@ -656,7 +656,6 @@ with tabs[4]:
             agrupar_por = b_col3.selectbox("Agrupar por (Opcional)", ["-- Agrupar resultados por categorías (opcional) --"] + list(df_pool.columns))
             var_analisis = b_col1.selectbox("Variable", ["-- seleccionar --"] + list(df_pool.columns), key="query_var")
             operacion = b_col2.selectbox("Operación", ["-- seleccionar --", "Conteo (Total)", "Conteo Único (Pacientes)", "Suma", "Promedio", "Mínimo", "Máximo"], key="query_operacion")
-            agrupar_por = b_col3.selectbox("Agrupar por (Opcional)", ["-- Agrupar resultados por categorías (opcional) --"] + list(df_pool.columns), key="query_agrupar")
             # --- BOTÓN DE RESET COMPLETO ---
             with st.container(border=True):
                 st.markdown("#### 🔄 Reiniciar Consulta Completa")
